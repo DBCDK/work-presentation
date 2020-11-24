c3ac7e634f2fd68902fd5b2916c8544ac310d9c0
Morten Bøgeskov <mb@dbc.dk>
2020-11-24 14:10:22 +0100

    Speeling

--- worker/src/main/java/dk/dbc/search/work/presentation/worker/WorkTreeBuilder.java
+++ worker/src/main/java/dk/dbc/search/work/presentation/worker/WorkTreeBuilder.java
@@ -105,7 +105,7 @@ public class WorkTreeBuilder {
                 if (type.isPresentable()) {
                     ids.stream()
                             .filter(s -> s.startsWith("unit:")) // This can be removed when corepo has been cleaned up for bad relations
-                            .filter(contentService::objectIsActive) // This is a hack, because some relations hasn't been removed when they were deleted
+                            .filter(contentService::objectIsActive) // This is a hack, because some relations haven't been removed when they were deleted
                             .forEach(r -> unitTree.addRelation(type, r));
                 }
             });
