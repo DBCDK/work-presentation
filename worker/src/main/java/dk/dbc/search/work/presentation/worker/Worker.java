41675cdf3147d6f0254e06dfdbb77699087f1b9b
Morten Bøgeskov <mb@dbc.dk>
2020-12-04 12:12:55 +0100

    PMD

--- worker/src/main/java/dk/dbc/search/work/presentation/worker/Worker.java
+++ worker/src/main/java/dk/dbc/search/work/presentation/worker/Worker.java
@@ -29,7 +29,6 @@ import java.util.concurrent.ExecutorService;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
-import javax.ejb.EJBException;
 import javax.ejb.Lock;
 import javax.ejb.LockType;
 import javax.ejb.Singleton;
