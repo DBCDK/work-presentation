b1109f23d89dc576608c8f4e853913e710c6bfdd
Morten Bøgeskov <mb@dbc.dk>
2020-12-08 13:30:55 +0100

    Remove unused manifestations from cache

--- worker/src/main/java/dk/dbc/search/work/presentation/worker/WorkConsolidator.java
+++ worker/src/main/java/dk/dbc/search/work/presentation/worker/WorkConsolidator.java
@@ -82,7 +82,8 @@ public class WorkConsolidator {
     public void deleteWork(String corepoWorkId) {
         WorkObjectEntity work = WorkObjectEntity.fromCorepoWorkId(em, corepoWorkId);
         if (work != null) {
-            WorkContainsEntity.listFrom(em, corepoWorkId)
+            List<WorkContainsEntity> oldWorkContainsList = WorkContainsEntity.listFrom(em, corepoWorkId);
+            oldWorkContainsList
                     .stream()
                     .map(WorkContainsEntity::getManifestationId)
                     .forEach(manifestationId -> {
@@ -104,6 +105,9 @@ public class WorkConsolidator {
      */
     @Timed(reusable = true)
     public void saveWork(String corepoWorkId, WorkTree tree, WorkInformation content) {
+
+        setWorkContains(tree);
+
         String persistentWorkId = tree.getPersistentWorkId();
         WorkObjectEntity work = WorkObjectEntity.from(em, persistentWorkId);
         WorkObjectEntity workByWorkId = WorkObjectEntity.fromCorepoWorkId(em, corepoWorkId);
@@ -141,7 +145,6 @@ public class WorkConsolidator {
     public WorkInformation buildWorkInformation(WorkTree tree) {
         WorkInformation work = new WorkInformation();
 
-        setWorkContains(tree);
         Map<String, ManifestationInformation> manifestationCache = buildManifestationCache(tree);
 
         work.workId = tree.getPersistentWorkId();
@@ -149,7 +152,7 @@ public class WorkConsolidator {
         // Copy from owner
         String ownerId = tree.getPrimaryManifestationId();
         ManifestationInformation primary = manifestationCache.get(ownerId);
-        if(primary == null) {
+        if (primary == null) {
             throw new IllegalStateException("primary: " + ownerId + " could not be resolved");
         }
         work.creators = TypedValue.distinctSet(primary.creators);
@@ -243,9 +246,21 @@ public class WorkConsolidator {
         return manifestationCollection.getManifestations();
     }
 
+    /**
+     * Given a tree, set the workcontains list, and remove orphaned cache
+     * entries
+     *
+     * @param tree current work description
+     */
     void setWorkContains(WorkTree tree) {
         String corepoWorkId = tree.getCorepoWorkId();
         Set<String> activeManifestationIds = tree.extractManifestationIds();
+
+        WorkContainsEntity.listFrom(em, corepoWorkId).stream()
+                .map(WorkContainsEntity::getManifestationId)
+                .filter(m -> !activeManifestationIds.contains(m))
+                .forEach(m -> CacheEntity.from(em, m).delete());
+
         List<WorkContainsEntity> workContains = activeManifestationIds.stream()
                 .map(m -> WorkContainsEntity.from(em, corepoWorkId, m))
                 .collect(toList());
